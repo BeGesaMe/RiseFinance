9eee4329d63f4252fba45f9c2c0e3a556a1a2289
BeGesaMe <bekjonshavkatov0@gmail.com>
2024-04-15 23:15:53 +0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+let consultationBox = document.querySelector('.consultation-box');
+let body = document.querySelector('body');
+
+
+// oper window
+function consultation(){
+    consultationBox.style = "display: block; position: fixed; z-index: 888";
+}
+
+// close window
+function close1(){
+    consultationBox.style = "display: none"
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#121139',
+        'textBlue': '#812DE2'
+      },
+      fonts:{
+        'popins': 'Poppins'
+      }
+    },
+  },
+  plugins: [],
+}
